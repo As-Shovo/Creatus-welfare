5477ac72d966776ada297072fa9f5764c3e4b694
torisd560 <mitalisd123@gmail.com>
2022-01-02 18:18:28 +0600

    [MOD]-header

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,110 +1,110 @@
-import {
-    createUserWithEmailAndPassword,
-    getAuth,
-    GoogleAuthProvider,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    signOut,
-    updateProfile,
-} from "firebase/auth";
-import { useEffect, useState } from "react";
-import initAuth from "../Firebase/firebase.init";
-initAuth();
-const useFirebase = () => {
-    const auth = getAuth();
-    const [user, setUser] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    // const [admin, setAdmin] = useState(false);
-    const logInUsingGoogle = (history, location) => {
-        setIsLoading(true);
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                setUser(result.user)
+// import {
+//     createUserWithEmailAndPassword,
+//     getAuth,
+//     GoogleAuthProvider,
+//     onAuthStateChanged,
+//     signInWithEmailAndPassword,
+//     signInWithPopup,
+//     signOut,
+//     updateProfile,
+// } from "firebase/auth";
+// import { useEffect, useState } from "react";
+// import initAuth from "../Friebase/firebase.init";
+// initAuth();
+// const useFirebase = () => {
+//     const auth = getAuth();
+//     const [user, setUser] = useState({});
+//     const [isLoading, setIsLoading] = useState(true);
+//     // const [admin, setAdmin] = useState(false);
+//     const logInUsingGoogle = (history, location) => {
+//         setIsLoading(true);
+//         const provider = new GoogleAuthProvider();
+//         signInWithPopup(auth, provider)
+//             .then((result) => {
+//                 setUser(result.user)
 
-            })
-            .then(() => {
-                const destination = location?.state?.from || "/";
-                history.replace(destination);
-            })
-            .catch((error) => {
-                console.log(error.message);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    };
+//             })
+//             .then(() => {
+//                 const destination = location?.state?.from || "/";
+//                 history.replace(destination);
+//             })
+//             .catch((error) => {
+//                 console.log(error.message);
+//             })
+//             .finally(() => {
+//                 setIsLoading(false);
+//             });
+//     };
 
-    const signUpWithEmailAndPassword = (email, password, name, history) => {
-        setIsLoading(true);
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((result) => {
-                // saveUser(email, name, "POST");
-                updateProfile(auth.currentUser, {
-                    displayName: name,
-                });
-                history.replace("/");
+//     const signUpWithEmailAndPassword = (email, password, name, history) => {
+//         setIsLoading(true);
+//         createUserWithEmailAndPassword(auth, email, password)
+//             .then((result) => {
+//                 // saveUser(email, name, "POST");
+//                 updateProfile(auth.currentUser, {
+//                     displayName: name,
+//                 });
+//                 history.replace("/");
 
-                console.log(result.user);
-            })
+//                 console.log(result.user);
+//             })
 
-            .catch((error) => {
-                console.log(error.message);
-            })
-            .finally(() => setIsLoading(false));
-    };
-    const emailAndPasswordSignIn = (email, password, location, history) => {
-        setIsLoading(true);
-        signInWithEmailAndPassword(auth, email, password)
-            .then((result) => {
-                // Signed in
-                // ...
+//             .catch((error) => {
+//                 console.log(error.message);
+//             })
+//             .finally(() => setIsLoading(false));
+//     };
+//     const emailAndPasswordSignIn = (email, password, location, history) => {
+//         setIsLoading(true);
+//         signInWithEmailAndPassword(auth, email, password)
+//             .then((result) => {
+//                 // Signed in
+//                 // ...
 
-                const destination = location?.state?.from || "/";
-                history.replace(destination);
+//                 const destination = location?.state?.from || "/";
+//                 history.replace(destination);
 
-                console.log(result.user);
-            })
-            .catch((error) => {
-                const errorMessage = error.message;
-                console.log(errorMessage);
-            })
-            .finally(() => setIsLoading(false));
-    };
+//                 console.log(result.user);
+//             })
+//             .catch((error) => {
+//                 const errorMessage = error.message;
+//                 console.log(errorMessage);
+//             })
+//             .finally(() => setIsLoading(false));
+//     };
 
 
-    const logOut = () => {
-        signOut(auth)
-            .then(() => {
-                // Sign-out successful.
-            })
-            .catch((error) => {
-                // An error happened.
-                console.log(error.message);
-            })
-            .finally(() => setIsLoading(false));
-    };
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser(user);
-            } else {
+//     const logOut = () => {
+//         signOut(auth)
+//             .then(() => {
+//                 // Sign-out successful.
+//             })
+//             .catch((error) => {
+//                 // An error happened.
+//                 console.log(error.message);
+//             })
+//             .finally(() => setIsLoading(false));
+//     };
+//     useEffect(() => {
+//         onAuthStateChanged(auth, (user) => {
+//             if (user) {
+//                 setUser(user);
+//             } else {
                
-                setUser({});
-            }
-            setIsLoading(false);
-        });
-    }, [auth]);
+//                 setUser({});
+//             }
+//             setIsLoading(false);
+//         });
+//     }, [auth]);
 
-    return {
-        logInUsingGoogle,
-        signUpWithEmailAndPassword,
-        emailAndPasswordSignIn,
-        logOut,
-        user,
-        setUser,
-        isLoading,
-    };
-};
-export default useFirebase;
+//     return {
+//         logInUsingGoogle,
+//         signUpWithEmailAndPassword,
+//         emailAndPasswordSignIn,
+//         logOut,
+//         user,
+//         setUser,
+//         isLoading,
+//     };
+// };
+// export default useFirebase;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,7 +5,7 @@ import useAuth from '../../../Hooks/useAuth'
 
 const Header = () => {
 
-    const {user , logOut} = useAuth()
+    // const {user , logOut} = useAuth()
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='p-2'>
@@ -18,14 +18,14 @@ const Header = () => {
                 <Nav className="ms-auto nav-item">
                     <Nav.Link as={Link} to='/home'>Home</Nav.Link>
                     <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
-                    {user.email &&
+                    {/* {user.email &&
                         <div className=' d-flex justify-content-between align-items-center'>
                             <img src={user.photoURL} alt="" className=' img-fluid' style={{ width: '40px', borderRadius: '50%' }} />
                             <span className='fw-bold custom-text-primary ms-2'>{user.displayName}</span>
                         </div>}
                     {user.email ? <span onClick={logOut} className=' custom-cursor '><i className="fas fa-sign-out-alt custom-text-primary me-2 fs-5 "></i>LogOut</span>
                         :
-                        <Nav.Link as={Link} to='/login'><i className="fas fa-sign-in-alt custom-text-primary me-2 fs-5"></i>Login</Nav.Link>}
+                        <Nav.Link as={Link} to='/login'><i className="fas fa-sign-in-alt custom-text-primary me-2 fs-5"></i>Login</Nav.Link>} */}
                 </Nav>
             </Navbar.Collapse>
         </Container>
