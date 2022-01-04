3c6a3b8043fe9f49c57cee30856a8660a2ae7ef7
torisd560 <mitalisd123@gmail.com>
2022-01-05 00:13:35 +0600

    [ADD]- footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,28 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import Footer from '../Shared/Footer/Footer';
-=======
 import Header from '../Shared/Header/Header';
 import Banner from './Banner/Banner';
 import BecomeVolunteer from './BecomeVolunteer/BecomeVolunteer'
-import Causes from './Caueses/Causes';
-import Volunteers from './Volunteers/Vlounteers'
->>>>>>> 8304a3490a15725901f75c71e418a49103cda307
+import Volunteers from './Volunteers'
 
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
-            <h1>THis is home</h1>
-            <h2> Hero Section change </h2>
-            <Footer></Footer>
-=======
             <Header></Header>
             <Banner></Banner>
-            <Causes></Causes>
             <BecomeVolunteer></BecomeVolunteer>
             <Volunteers></Volunteers>
->>>>>>> 8304a3490a15725901f75c71e418a49103cda307
         </div>
     );
 };
