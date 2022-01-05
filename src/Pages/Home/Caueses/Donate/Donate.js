4abd7de2c5e05f79218c8ffe6561905ba53de842
As-Shovo <akbarshovo18@gmail.com>
2022-01-05 14:01:53 +0600

    [donated data single data find ]

--- src/Pages/Home/Caueses/Donate/Donate.js
+++ src/Pages/Home/Caueses/Donate/Donate.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import causesData from '../../../../fakeData/donationServices.json';
 
 const Donate = () => {
     const { id } = useParams();
+    const selectedData = causesData.filter(sd => sd.id == id);
+    console.log(selectedData);
     return (
         <div>
             <h2>Donate id {id} </h2>
